d8f49e8052aae99551b322d5d77bbbacef43e686
franek <franek.stodulski@gmail.com>
2018-08-27 16:17:02 +0200

    TypeScript, Less, Creted Fetching reuseable function, created POST prototype component

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.less';
+
+import Post from './components/post/post';
+
+interface MainProps {}
+
+interface MainState {}
+class App extends Component<MainProps, MainState> {
+  constructor(props: MainProps) {
+    super(props);
+  }
+
+  public componentDidMount() {}
+  public render() {
+    return (
+      <div className="App">
+        <Post />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/post/post.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { Fetch } from '../../helpers/fetch';
+
+// Interface for single post object;
+interface IPost {
+  id: number;
+  authorId: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// Interface for props
+interface PostProps {
+  name?: string;
+}
+
+// Interface for state
+interface PostState {
+  postsArray: IPost[];
+}
+
+// Post class
+class Post extends Component<PostProps, PostState> {
+  constructor(props: PostProps, private fetch: Fetch) {
+    super(props);
+    // Init state object
+    this.state = {
+      postsArray: []
+    };
+    // Create new instance of Fetch
+    this.fetch = new Fetch();
+  }
+
+  //   Hook for mount event
+  public componentDidMount = () => {
+    // Send GET request
+    const postsList = this.fetch.request(
+      'http://localhost:8081/api/v1/secured/posts',
+      {
+        method: 'GET'
+      }
+    );
+
+    // Menage Promise
+    postsList.then(response => {
+      this.setState({
+        postsArray: [...response]
+      });
+    });
+  };
+
+  //   Render DOM
+  public render() {
+    return (
+      <div className="post">
+        <ul>
+          {this.state.postsArray.map(post => (
+            <li>
+              <div key={post.id}>{post.content}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Post;
