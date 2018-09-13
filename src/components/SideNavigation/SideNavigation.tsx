f0c9e90ff63418096028c1e2e11bd86df32b4226
franek <franek.stodulski@gmail.com>
2018-09-13 11:49:57 +0200

    resolved conflicts

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import './SideNavigation.scss';
 import { DefaultButton } from '../common/button';
 
-const SideNavigation = () => (
+export const SideNavigation = () => (
   <div className="navigation__side">
     <DefaultButton />
   </div>
