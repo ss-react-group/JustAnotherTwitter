d8b0752289675c03640f1a63016b4c6867b31b0e
Michał Parkitny <kontakt@michalparkitny.com>
2018-09-17 12:13:35 +0200

    Fixed console error about redirect to the current path.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
-import * as React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import SecuredRoute from './components/SecuredRoute';
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import SecuredRoute from "./components/SecuredRoute";
 
-import './App.scss';
-import './assets/styles/common.scss';
+import "./App.scss";
+import "./assets/styles/common.scss";
 
-import { MainLayout } from './components/Layouts/MainLayout';
-import { Authorization } from './pages/Authorization';
+import { MainLayout } from "./components/Layouts/MainLayout";
+import { Authorization } from "./pages/Authorization";
 
-import { getAsset } from './services/asset';
-import { IAsset } from './interfaces/asset';
-import { observer, inject } from 'mobx-react';
+import { getAsset } from "./services/asset";
+import { IAsset } from "./interfaces/asset";
+import { observer, inject } from "mobx-react";
 
 interface IAppProps {
   stores?: any;
 }
 
-@inject('stores')
+@inject("stores")
 @observer
 export default class App extends React.Component<IAppProps, {}> {
   constructor(props: IAppProps) {
     super(props);
-    const userDetails = JSON.parse(localStorage.getItem('userDetails'));
+    const userDetails = JSON.parse(localStorage.getItem("userDetails"));
     this.props.stores.userDetails.user = userDetails;
   }
 
   componentDidMount() {
     getAsset(2, 1).then((result: IAsset) => {
-      this.props.stores.assets.set('avatar', result.filePath);
+      this.props.stores.assets.set("avatar", result.filePath);
     });
 
     getAsset(2, 2).then((result: IAsset) => {
-      this.props.stores.assets.set('background', result.filePath);
+      this.props.stores.assets.set("background", result.filePath);
     });
   }
 
@@ -39,14 +44,18 @@ export default class App extends React.Component<IAppProps, {}> {
     return (
       <div className="App">
         <Router>
-          <div className="router">
-            {this.props.stores.userDetails.user ? <Redirect to="/home"/> : <Route exact path="/" component={Authorization} />}
+          <Switch>
             <SecuredRoute
               component={MainLayout}
               path="/home"
               guard={this.props.stores.userDetails.user}
             />
-          </div>
+            {this.props.stores.userDetails.user ? (
+              <Redirect to="/home" />
+            ) : (
+              <Route exact path="/" component={Authorization} />
+            )}
+          </Switch>
         </Router>
       </div>
     );
