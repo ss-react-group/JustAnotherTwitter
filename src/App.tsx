b4f1fdbf0ed7a8d8c9cc6e9d2edaeb30ade53fe3
Daria Daletska <dpys@franz.com>
2018-09-18 08:08:31 +0200

    Fix encrypting

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,13 @@ interface IAppProps {
 export default class App extends React.Component<IAppProps, {}> {
   constructor(props: IAppProps) {
     super(props);
+    /*******just for the first launch after change***********/
+    localStorage.removeItem('userDetails');
+    localStorage.removeItem('token');
+    /******************************************/
     const userDetails = JSON.parse(localStorage.getItem('userDetails'));
-    this.props.stores.userDetails.user = userDetails;
+    console.log(userDetails);
+    userDetails && (this.props.stores.userDetails.user = userDetails);
   }
 
   render() {
