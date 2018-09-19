839fbac16b4f88e28252eba59ce41da5b83fb98f
Michał <kontakt@michalparkitny.com>
2018-09-19 17:49:59 +0200

    Layout fix. (#31)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,10 @@
 import * as React from 'react';
-import { Navigation } from '../../components/Navigation';
-import { BackgroundImage } from '../../components/BackgroundImage';
-import { Info } from '../../components/Info';
 import { TweetList } from '../../components/TweetList';
 import { Followers } from '../../components/Followers';
-import { ProgressBar } from '../../components/ProgressBar';
 
 export const Home = () => (
   <React.Fragment>
-    <ProgressBar />
-    <Navigation />
-    <BackgroundImage />
-    <Info />
-    <div className="content container">
-      <TweetList />
-      <Followers />
-    </div>
+    <TweetList />
+    <Followers />
   </React.Fragment>
 );
