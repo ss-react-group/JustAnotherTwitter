9ecad1aaa7b833720ecba1b438ae2ee1df9c9eb6
Michał <kontakt@michalparkitny.com>
2018-09-19 15:38:31 +0200

    Refactor: Layout (#29)
    * Navigation refactored.
    
    * Implemented one AppRoute component for secured and not-secured routes.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { MainNavigation } from '../MainNavigation';
-import { SideNavigation } from '../SideNavigation';
+import { Main } from './Main';
+import { Side } from './Side';
 import { Search } from '../Search';
+
 
 import './Navigation.scss';
 
@@ -11,9 +12,9 @@ export const Navigation = () => (
   <header>
     <div className="container">
       <div className="navigation-grid">
-        <MainNavigation />
+        <Main />
         <Search />
-        <SideNavigation />
+        <Side />
       </div>
     </div>
   </header>
--- src/components/SecuredRoute/index.ts
+++
@@ -1,1 +0,0 @@
-export { SecuredRoute } from './SecuredRoute';
