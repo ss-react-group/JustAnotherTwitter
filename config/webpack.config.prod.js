005a7659cf3620919d71b782c3ace24ac88b43ba
franek <franek.stodulski@gmail.com>
2018-09-21 10:36:32 +0200

    added scss loader on prod webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -144,6 +144,14 @@ module.exports = {
               compact: true,
             },
           },
+          {
+            test: /\.scss$/,
+            use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+          },
           // Compile .tsx?
           {
             test: /\.(ts|tsx)$/,
